45f6580fbd82d9bd29a0cc06cab7da7abd86a39c
Ricard Sanchez Besalduch, 1426118 <ricard.sanchezBe@e-campus.uab.cat>
2020-11-07 10:55:59 +0100

    Updates shoot() test, by testing also exceptions

--- src/test/java/controller/PlayerTest.java
+++ src/test/java/controller/PlayerTest.java
@@ -1,18 +1,18 @@
 package controller;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
+	private Player player;
 	/*
 	 * Here we are doing decision coverage. 
 	 */
 	@Test
 	public void shootTest() {
 		int[][] board;
-		Player player;
 		
 		player = new MockPlayer();
 		board = new int[][] {{0}};
@@ -25,5 +25,17 @@ public class PlayerTest {
 		player.setOpponent(new MockPlayer(board, null));
 		player.shoot(0, 0);
 		assertEquals(board[0][0], 4);
+		
+		player = new MockPlayer();
+		board = new int[][] {{1}};
+		player.setOpponent(new MockPlayer(board, null));
+		player.shoot(0, 0);
+		assertThrows(Exception.class, () -> player.shoot(0, 0));
+		
+		player = new MockPlayer();
+		board = new int[][] {{4}};
+		player.setOpponent(new MockPlayer(board, null));
+		player.shoot(0, 0);
+		assertThrows(Exception.class, () -> player.shoot(0, 0));
 	}
 }
