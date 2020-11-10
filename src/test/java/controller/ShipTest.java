180f0698df81fa35f217a36167390a1a3f55156e
Ricard Sanchez Besalduch, 1426118 <ricard.sanchezBe@e-campus.uab.cat>
2020-11-10 18:43:05 +0100

    Adds test for fitsInBoard()

--- src/test/java/controller/ShipTest.java
+++ src/test/java/controller/ShipTest.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package battleship;
 
 import static org.junit.Assert.*;
@@ -23,4 +24,49 @@ public class ShipTest {
 		
 	}
 
+=======
+package controller;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ShipTest {
+	/*
+	 * Here we are doing decision coverage, at first. And condition coverage
+	 * later. 
+	 */
+	@Test
+	public void fitsInBoardTest() {
+		Ship ship = new Ship(5);
+		int m = 10, n = 10;
+		
+		// Decision coverage
+		ship.setOrientation(Orientation.VERTICAL);
+		ship.setPosition(5, 0);
+		assertTrue(ship.fitsInBoard(m, n));
+		
+		ship.setOrientation(Orientation.VERTICAL);
+		ship.setPosition(6, 0);
+		assertFalse(ship.fitsInBoard(m, n));
+		
+		ship.setOrientation(Orientation.HORIZONTAL);
+		ship.setPosition(0, 5);
+		assertTrue(ship.fitsInBoard(m, n));
+		
+		ship.setOrientation(Orientation.HORIZONTAL);
+		ship.setPosition(0, 6);
+		assertFalse(ship.fitsInBoard(m, n));
+		
+		// Condition coverage
+		ship.setOrientation(Orientation.VERTICAL);
+		ship.setPosition(5, 0);
+		assertTrue(ship.fitsInBoard(m, n));
+		
+		ship.setOrientation(Orientation.VERTICAL);
+		ship.setPosition(6, 0);
+		assertFalse(ship.fitsInBoard(m, n));
+	}
+>>>>>>> ff3e964... Adds test for fitsInBoard()
 }
