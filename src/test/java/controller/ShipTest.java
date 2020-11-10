e6ee794ea9599f30ef371c0c2b966f72158bf262
Ricard Sanchez Besalduch, 1426118 <ricard.sanchezBe@e-campus.uab.cat>
2020-11-10 19:33:07 +0100

    Updates tests for fitsInBoard()

--- src/test/java/controller/ShipTest.java
+++ src/test/java/controller/ShipTest.java
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-package battleship;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-public class ShipTest {
-
-	@Test
-	public void TestisSunk() {
-		
-		Ship ship;
-		
-		//case n = 0
-		ship = new MockShip(0,false);
-		assertFalse(ship.isSunk());
-		// case n = 1 false
-		ship = new MockShip(0,true);
-		assertFalse(ship.isSunk());
-		// case n = 1 true
-		ship = new MockShip(1,false);
-		assertFalse(ship.isSunk());
-		
-	}
-
-=======
 package controller;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -39,10 +12,10 @@ public class ShipTest {
 	 */
 	@Test
 	public void fitsInBoardTest() {
+		// Decision coverage
 		Ship ship = new Ship(5);
 		int m = 10, n = 10;
 		
-		// Decision coverage
 		ship.setOrientation(Orientation.VERTICAL);
 		ship.setPosition(5, 0);
 		assertTrue(ship.fitsInBoard(m, n));
@@ -60,13 +33,39 @@ public class ShipTest {
 		assertFalse(ship.fitsInBoard(m, n));
 		
 		// Condition coverage
+		ship = new Ship(11);
+		m = 20; n = 20;
+		
 		ship.setOrientation(Orientation.VERTICAL);
-		ship.setPosition(5, 0);
+		ship.setPosition(11, 0);
 		assertTrue(ship.fitsInBoard(m, n));
 		
 		ship.setOrientation(Orientation.VERTICAL);
-		ship.setPosition(6, 0);
+		ship.setPosition(12, 0);
+		assertFalse(ship.fitsInBoard(m, n));
+		
+		ship.setOrientation(Orientation.HORIZONTAL);
+		ship.setPosition(0, 11);
+		assertTrue(ship.fitsInBoard(m, n));
+		
+		ship.setOrientation(Orientation.HORIZONTAL);
+		ship.setPosition(0, 12);
 		assertFalse(ship.fitsInBoard(m, n));
 	}
->>>>>>> ff3e964... Adds test for fitsInBoard()
+	@Test
+	public void TestisSunk() {
+		
+		Ship ship;
+		
+		//case n = 0
+		ship = new MockShip(0,false);
+		assertFalse(ship.isSunk());
+		// case n = 1 false
+		ship = new MockShip(0,true);
+		assertFalse(ship.isSunk());
+		// case n = 1 true
+		ship = new MockShip(1,false);
+		assertFalse(ship.isSunk());
+		
+	}
 }
