59963b66e0683f7b216a376664b43f68f7a1bbbe
Hugo Rodriguez <hugo.rodriguezp@e-campus.uab.cat>
2020-11-15 10:52:12 +0100

    AndroidPlayer restTest and aimTest

--- src/test/java/controller/AndroidPlayerTest.java
+++ src/test/java/controller/AndroidPlayerTest.java
@@ -7,18 +7,31 @@ import org.junit.jupiter.api.Test;
 class AndroidPlayerTest {
 
 	@Test
-	public void androidPlayerTest() {
+	public void restTest() {
 		AndroidPlayer player;
-		List<Ship> shipList= Arrays.asList(new Ship(1));
-		player = new AndroidPlayer(3,3,shipList);
-		Ship shipExample[] = {new Ship(1)};
-		int[][] boardExample = new int[][] {{0,0,0},{0,0,0},{0,0,0}};
-		
-		assertFalse(player.canGraceShot);
-		assertFalse(player.graceShotHit);
-		assertArrayEquals(player.ships.toArray(),shipExample);
-		assertArrayEquals(player.board,boardExample);
-		
+		List<Ship> shipList= Arrays.asList(new Ship(3),new Ship(1),new Ship(1),new Ship(4));
+		player = new AndroidPlayer(8,8,shipList);	
 	} 
+	
+	@Test
+	public void aimTest() {
+		AndroidPlayer player;
+		Player player2;
+		List<Ship> shipList= Arrays.asList(new Ship(1),new Ship(1));
+		List<Ship> shipList2= Arrays.asList(new Ship(2),new Ship(2));
+		int[][] board2= {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
+		player = new AndroidPlayer(4,4,shipList);	
+		player2 = new MockPlayer(board2,shipList2);
+		player.setOpponent(player2);
+		
+		player2.ships.get(0).setPosition(0, 0);
+		player2.setShip(board2, player.ships.get(0), 1);
+			
+		Coord coord=player.aim();
+		player.shoot(coord.i,coord.j);
+
+		
+	
+	}
 
 }
