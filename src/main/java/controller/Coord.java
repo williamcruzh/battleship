0b2e2bd003ddbdab528cfe1a031505aa48000528
Ricard Sanchez Besalduch, 1426118 <ricard.sanchezBe@e-campus.uab.cat>
2020-11-16 19:09:13 +0100

    Updates methods for Coord class
    This commit updates methods of Coord() and Coord(int i, int j).

--- src/main/java/controller/Coord.java
+++ src/main/java/controller/Coord.java
@@ -7,11 +7,20 @@ public class Coord {
 	public int j;
 	
 	public Coord() {
-		this.i = -1; 
-		this.j = -1;
+		this.i = 0; 
+		this.j = 0;
 	}
 	public Coord(int i, int j) {
-		this.i = j; 
-		this.j = i;
+		this.i = i; 
+		this.j = j;
 	}
+	/*
+	 * TODO: Make it more efficient with a dict which has 
+	 *       Coord as key, and it has ship pointer as value.
+	 *       Only for coords where there is a ship.
+	@Override
+	public int hashCode() {
+		return Objects.hash(i, j);
+	}
+	*/
 }
