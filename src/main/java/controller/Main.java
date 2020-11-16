9607573b3197605458e9cfd7554069423c7996c8
Ricard Sanchez Besalduch, 1426118 <ricard.sanchezBe@e-campus.uab.cat>
2020-11-16 19:12:16 +0100

    Updates methods for Main class
    This commit updates the main() method, entry of the application.

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	static Graphics graphics;
 	
 	public static void main(String[] args) throws Exception {
-		int[] ships = new int[] {4, 3, 2, 1, 1};
+		int[] ships = new int[] {4, 3, 3, 3, 2, 2, 2, 1, 1};
 		Main.graphics = new BattleshipConsole(10, 10);
 		Game game = new Game(10, 10, ships);
 		game.play();
