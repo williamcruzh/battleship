c4a3a4982eb32fd6c73fc72aa238d347ef66c46f
Ricard Sanchez Besalduch, 1426118 <ricard.sanchezBe@e-campus.uab.cat>
2020-11-16 18:58:00 +0100

    Updates methods for HumanPlayer class
    This commit updates methods of positionShip() and aim().

--- src/main/java/controller/HumanPlayer.java
+++ src/main/java/controller/HumanPlayer.java
@@ -22,6 +22,9 @@ public class HumanPlayer extends Player {
 	public void positionShip(Ship ship) {
 		Main.graphics.drawCreationBoardsScreen(board);
 		Coord coord = Main.graphics.drawMoveShip(ship.getSize());
+		Orientation orientation = Main.graphics.drawRotateShip();
+		ship.setPosition(coord.i, coord.j);
+		ship.setOrientation(orientation);
 	}
 	public Coord aim() {
 		int i, j;
@@ -34,6 +37,14 @@ public class HumanPlayer extends Player {
 			// 4 equivalent partitions
 			if(i < 0 || j < 0 || i >= M || i >= N) {
 				Main.graphics.drawOutOfBoard();
+			}
+			else if(opposingPlayer.board[i][j] == 2 && 
+					opposingPlayer.board[i][j] == 3 && 
+					opposingPlayer.board[i][j] == 4) {
+				Main.graphics.drawSquareAlreadySelected();
+			}
+			else {
+				break;
 			}
 		}
 		return new Coord(i, j);
