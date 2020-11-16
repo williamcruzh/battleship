e08b07a95499a3c20194465eaa993c0abf0971ac
Ricard Sanchez Besalduch, 1426118 <ricard.sanchezBe@e-campus.uab.cat>
2020-11-16 19:10:56 +0100

    Adds methods for Main class
    This commit adds the main() method, entry of the application.

---
+++ src/main/java/controller/Main.java
@@ -0,0 +1,15 @@
+package controller;
+
+import view.BattleshipConsole;
+import view.Graphics;
+
+public class Main {
+	static Graphics graphics;
+	
+	public static void main(String[] args) throws Exception {
+		int[] ships = new int[] {4, 3, 2, 1, 1};
+		Main.graphics = new BattleshipConsole(10, 10);
+		Game game = new Game(10, 10, ships);
+		game.play();
+	}
+}
