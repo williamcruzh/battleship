caeab0afb702665f76513149b1d3d7367ac08e67
Ricard Sanchez Besalduch, 1426118 <ricard.sanchezBe@e-campus.uab.cat>
2020-11-16 19:07:32 +0100

    Adds methods for Coord class
    This commit adds methods of Coord() and Coord(int i, int j).

---
+++ src/main/java/controller/Coord.java
@@ -0,0 +1,17 @@
+package controller;
+
+import java.util.Objects;
+
+public class Coord {
+	public int i;
+	public int j;
+	
+	public Coord() {
+		this.i = -1; 
+		this.j = -1;
+	}
+	public Coord(int i, int j) {
+		this.i = j; 
+		this.j = i;
+	}
+}
